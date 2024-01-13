43220059299d3b33e7fe179040b3ac5120986de8
Ishan Singh <ishan.1999@gmail.com>
2024-01-13 10:55:25 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -346,6 +346,100 @@ const addFrameToImage = async (
 //   });
 // };
 
+// const addFrameToVideo = async (
+//   originalVideoPath,
+//   frameImagePath,
+//   outputVideoPath,
+//   filename,
+//   res,
+//   phone
+// ) => {
+//   const ffprobe = require("ffprobe");
+//   const ffprobePath = require("ffprobe-static").path;
+
+//   const localPath = `uploads/${filename}`;
+//   const localPathDir = path.dirname(localPath);
+//   await fs.promises.mkdir(localPathDir, { recursive: true });
+//   const localFilePath = path.resolve(__dirname, "..", localPath);
+//   await downloadFile(originalVideoPath, localFilePath);
+//   //downloading frame
+//   const localFramePath = `uploads/${phone}/${filename}`;
+//   const localFramePathDir = path.dirname(localFramePath);
+//   await fs.promises.mkdir(localFramePathDir, { recursive: true });
+//   const localFrameFilePath = path.resolve(__dirname, "..", localFramePath);
+//   await downloadFile(frameImagePath, localFrameFilePath);
+
+//   ffprobe(localFilePath, { path: ffprobePath }, (err, info) => {
+//     if (err) {
+//       console.error("Error getting video dimensions:", err);
+//       return;
+//     }
+//     const videoWidth = info.streams[0].width;
+//     const videoHeight = info.streams[0].height;
+//     const outputDirectory = path.dirname(outputVideoPath);
+
+//     fs.mkdirSync(outputDirectory, { recursive: true });
+
+//     ffmpeg()
+//       .input(localFrameFilePath)
+//       .videoFilters(`scale=${videoWidth}:${videoHeight}`)
+//       .output("resizedFrame.png")
+//       .on("end", () => {
+//         ffmpeg()
+//           .input(localFilePath)
+//           .input("resizedFrame.png")
+//           .complexFilter({
+//             filter: "overlay",
+//             options: {
+//               x: "W-w-10",
+//               y: "H-h-10",
+//             },
+//           })
+//           .output(outputVideoPath)
+//           .on("end", async () => {
+//             const cloudinaryOptions = {
+//               public_id: filename,
+//               folder: `media/${phone}/saved`,
+//               resource_type: "video",
+//             };
+//             let upload_result;
+//             try {
+//               upload_result = await cloudinary.uploader.upload(
+//                 outputVideoPath,
+//                 cloudinaryOptions
+//               );
+//             } catch (uploadError) {
+//               console.error("Error uploading to Cloudinary:", uploadError);
+//               return res.status(500).json({
+//                 success: false,
+//                 error: "Error uploading to Cloudinary.",
+//               });
+//             }
+
+//             const deleteVideoPath = path.join(
+//               __dirname,
+//               `../uploads/${phone}/saved`,
+//               filename
+//             );
+//             fs.unlinkSync(deleteVideoPath);
+//             return res.status(200).json({
+//               success: true,
+//               message: "Frame added successfully to video.",
+//               fileUrl: upload_result.secure_url,
+//             });
+//           })
+//           .on("error", (err) => {
+//             console.error("Error:", err);
+//           })
+//           .run();
+//       })
+//       .on("error", (err) => {
+//         console.error("Error resizing frame:", err);
+//       })
+//       .run();
+//   });
+// };
+
 const addFrameToVideo = async (
   originalVideoPath,
   frameImagePath,
@@ -362,7 +456,8 @@ const addFrameToVideo = async (
   await fs.promises.mkdir(localPathDir, { recursive: true });
   const localFilePath = path.resolve(__dirname, "..", localPath);
   await downloadFile(originalVideoPath, localFilePath);
-  //downloading frame
+
+  // Downloading frame
   const localFramePath = `uploads/${phone}/${filename}`;
   const localFramePathDir = path.dirname(localFramePath);
   await fs.promises.mkdir(localFramePathDir, { recursive: true });
@@ -374,67 +469,65 @@ const addFrameToVideo = async (
       console.error("Error getting video dimensions:", err);
       return;
     }
-    const videoWidth = info.streams[0].width;
-    const videoHeight = info.streams[0].height;
+
     const outputDirectory = path.dirname(outputVideoPath);
 
     fs.mkdirSync(outputDirectory, { recursive: true });
 
     ffmpeg()
+      .input(localFilePath)
       .input(localFrameFilePath)
-      .videoFilters(`scale=${videoWidth}:${videoHeight}`)
-      .output("resizedFrame.png")
-      .on("end", () => {
-        ffmpeg()
-          .input(localFilePath)
-          .input("resizedFrame.png")
-          .complexFilter({
-            filter: "overlay",
-            options: {
-              x: "W-w-10",
-              y: "H-h-10",
-            },
-          })
-          .output(outputVideoPath)
-          .on("end", async () => {
-            const cloudinaryOptions = {
-              public_id: filename,
-              folder: `media/${phone}/saved`,
-              resource_type: "video",
-            };
-            let upload_result;
-            try {
-              upload_result = await cloudinary.uploader.upload(
-                outputVideoPath,
-                cloudinaryOptions
-              );
-            } catch (uploadError) {
-              console.error("Error uploading to Cloudinary:", uploadError);
-              return res.status(500).json({
-                success: false,
-                error: "Error uploading to Cloudinary.",
-              });
-            }
-
-            const deleteVideoPath = path.join(
-              __dirname,
-              `../uploads/${phone}/saved`,
-              filename
-            );
-            fs.unlinkSync(deleteVideoPath);
-            return res.status(200).json({
-              success: true,
-              message: "Frame added successfully to video.",
-              fileUrl: upload_result.secure_url,
-            });
-          })
-          .on("error", (err) => {
-            console.error("Error:", err);
-          })
-          .run();
+      .complexFilter({
+        filter: "overlay",
+        options: {
+          x: "W-w-10",
+          y: "H-h-10",
+        },
+      })
+      .output(outputVideoPath)
+      .on("progress", (progress) => {
+        console.log("Processing: " + progress.percent + "% done");
       })
       .on("error", (err) => {
-        console.error("Error resizing frame:", err);
+        console.error("FFmpeg Error:", err);
+        res.status(500).json({
+          success: false,
+          error: "Error during video processing.",
+        });
+      })
+      .on("end", async () => {
+        const cloudinaryOptions = {
+          public_id: filename,
+          folder: `media/${phone}/saved`,
+          resource_type: "video",
+        };
+
+        let upload_result;
+        try {
+          upload_result = await cloudinary.uploader.upload(
+            outputVideoPath,
+            cloudinaryOptions
+          );
+        } catch (uploadError) {
+          console.error("Error uploading to Cloudinary:", uploadError);
+          return res.status(500).json({
+            success: false,
+            error: "Error uploading to Cloudinary.",
+          });
+        }
+
+        const deleteVideoPath = path.join(
+          __dirname,
+          `../uploads/${phone}/saved`,
+          filename
+        );
+        fs.unlinkSync(deleteVideoPath);
+
+        return res.status(200).json({
+          success: true,
+          message: "Frame added successfully to video.",
+          fileUrl: upload_result.secure_url,
+        });
       })
       .run();
   });
